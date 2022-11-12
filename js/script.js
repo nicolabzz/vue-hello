a4f7308e61f28ed53b2d32240bcabd9a08477b3b
nicolabzz <nbuzzigoli@gmail.com>
2022-11-12 21:43:29 +0100

    .All set

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */ 
-const app = new Vue ( {
-    el: '#root',
+const vm = new Vue ( { //questa e' la VUE ISTANCE (o VUE MODELL per il VMMV)
+    el: '#app',
+    data: {
+        h1Mess : 'Vue js is like MAGIC! Look at here!',
+        img : 'img/Angeli-Ribelli.png',
+        newContainerCall : 'wowLookAtThatBro'
+    }
 });
