c60ca78fc378fd6295ec6dd2282786cfb1404159
nicolabzz <nbuzzigoli@gmail.com>
2022-11-13 13:35:07 +0100

    Change pics

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const vm = new Vue ( { //questa e' la VUE ISTANCE (o VUE MODELL per il VMMV)
     el: '#app',
     data: {
         h1Mess : 'Vue js is like MAGIC! Look at here!',
-        img : 'img/Angeli-Ribelli.png',
+        img : 'img/Digital-art-head.jpg',
         newContainerCall : 'wowLookAtThatBro'
     }
 });
